1d88365d9a2af9cc897b3dcb9692bdc01d3803eb
Kostas <diamantopoulos.kostas@hotmail.com>
2024-06-01 23:53:20 +0100

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './Layout/navbar/navbar.component';
 import { FooterComponent } from './Layout/navbar/footer/footer.component';
+import { BlocksComponent } from './Layout/blocks/blocks/blocks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,FooterComponent],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent,BlocksComponent],
   templateUrl: './app.component.html',
   
 })
