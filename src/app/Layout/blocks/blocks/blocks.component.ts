f05565b828dcaed321c07ba91fb149db9087627b
Kostas <diamantopoulos.kostas@hotmail.com>
2024-06-02 21:23:15 +0100

    animation function

--- src/app/Layout/blocks/blocks/blocks.component.ts
+++ src/app/Layout/blocks/blocks/blocks.component.ts
@@ -1,6 +1,35 @@
 import { Component } from '@angular/core';
 import 'animate.css';
 
+
+// Function to check if an element is in the viewport
+function isInViewport(element: HTMLElement): boolean {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+let animationApplied = false;
+
+function addAnimation(): void {
+  const element = document.getElementById('animatedDiv');
+  if (element && isInViewport(element) && !animationApplied) {
+    element.classList.add('animate__animated', 'animate__fadeInLeft');
+    animationApplied = true;
+  }
+}
+
+
+
+// Event listener to trigger the addAnimation function on scroll
+window.addEventListener('scroll', addAnimation);
+
+
+
 @Component({
   selector: 'app-blocks',
   standalone: true,
@@ -8,6 +37,7 @@ import 'animate.css';
   templateUrl: './blocks.component.html',
   styleUrl: './blocks.component.css'
 })
+
 export class BlocksComponent {
 
 }
