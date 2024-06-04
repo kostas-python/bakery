3b55430c2311ed7ead518c66aca79a582dd621f3
Kostas <diamantopoulos.kostas@hotmail.com>
2024-06-04 20:43:37 +0100

    added prime ng

--- src/app/Layout/blocks/blocks/blocks.component.ts
+++ src/app/Layout/blocks/blocks/blocks.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import 'animate.css';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+
+
 
 
 @Component({
   selector: 'app-blocks',
   standalone: true,
-  imports: [],
+  imports: [AnimateOnScrollModule,],
   templateUrl: './blocks.component.html',
   styleUrl: './blocks.component.css'
 })
